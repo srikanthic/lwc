e8a6e4ed90d41a24a73a6267c77c42942dc436c2
Pierre-Marie Dartus <p.dartus@salesforce.com>
2021-02-25 10:10:50 +0100

    feat(engine-core): introduce sanitizeHtmlContent hook

--- packages/@lwc/template-compiler/src/parser/index.ts
+++ packages/@lwc/template-compiler/src/parser/index.ts
@@ -5,7 +5,6 @@
  * For full license text, see the LICENSE file in the repo root or https://opensource.org/licenses/MIT
  */
 import * as parse5 from 'parse5';
-import { hasOwnProperty } from '@lwc/shared';
 
 import {
     CompilerDiagnostic,
@@ -54,6 +53,8 @@ import {
     IRText,
     LWCDirectiveDomMode,
     LWCDirectiveRenderMode,
+    TemplateExpression,
+    TemplateParseResult,
     LWCDirectives,
     TemplateIdentifier,
     TemplateParseResult,
